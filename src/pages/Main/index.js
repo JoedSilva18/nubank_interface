d9520be8b23652c183d77c5fbeb38bbdde5d1ffb
Joed Silva <joedsilva2016@gmail.com>
2020-01-16 20:22:32 -0300

    Adicionando animacao no card

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Animated } from 'react-native';
-import { PanGestureHandler } from 'react-native-gesture-handler';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import Header from '~/components/Header';
 import Tabs from '~/components/Tabs';
@@ -16,37 +16,85 @@ import {
   CardFooter,
   Annotation
 } from './styles';
-import { from } from 'rxjs';
 
 export default function Main() {
+  let offset = 0;
+  const translateY = new Animated.Value(0);
+
+  const animatedEvent = Animated.event(
+    [
+      {
+        nativeEvent: {
+          translationY: translateY
+        }
+      }
+    ],
+    { useNativeDriver: true }
+  );
+
+  function onHandlerStateChange(event) {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      let opened = false;
+      const { translationY } = event.nativeEvent;
+
+      offset += translationY;
+
+      if (translationY >= 100) {
+        opened = true;
+      } else {
+        translateY.setValue(offset);
+        translateY.setOffset(0);
+        offset = 0;
+      }
+
+      Animated.timing(translateY, {
+        toValue: opened ? 380 : 0,
+        duration: 100,
+        useNativeDriver: true
+      }).start(() => {
+        offset = opened ? 380 : 0;
+        translateY.setOffset(offset);
+        translateY.setValue(0);
+      })
+    }
+  }
+
   return (
     <Container>
       <Header />
       <Content>
-        <Menu />
+        <Menu translateY={translateY} />
 
         <PanGestureHandler
-          onGestureEvent={}
+          onGestureEvent={animatedEvent}
           onHandlerStateChange={onHandlerStateChange}
         >
-        <Card>
-          <CardHeader>
-            <Icon name="attach-money" size={28} color="#666" />
-            <Icon name="visibility-off" size={28} color="#666" />
-          </CardHeader>
-          <CardContent>
-            <Title>Saldo disponivel</Title>
-            <Description>R$ 197.611,65</Description>
-          </CardContent>
-          <CardFooter>
-            <Annotation>
-              Transferência de R$ 20,00 recebida de Joed Silva as 06:00h
+          <Card style={{
+            transform: [{
+              translateY: translateY.interpolate({
+                inputRange: [-200, 0, 380],
+                outputRange: [-50, 0, 380],
+                extrapolate: 'clamp'
+              })
+            }]
+          }}>
+            <CardHeader>
+              <Icon name="attach-money" size={28} color="#666" />
+              <Icon name="visibility-off" size={28} color="#666" />
+            </CardHeader>
+            <CardContent>
+              <Title>Saldo disponivel</Title>
+              <Description>R$ 197.611,65</Description>
+            </CardContent>
+            <CardFooter>
+              <Annotation>
+                Transferência de R$ 20,00 recebida de Joed Silva as 06:00h
             </Annotation>
-          </CardFooter>
-        </Card>
+            </CardFooter>
+          </Card>
         </PanGestureHandler>
       </Content>
-      <Tabs />
+      <Tabs translateY={translateY} />
     </Container>
   )
 };
